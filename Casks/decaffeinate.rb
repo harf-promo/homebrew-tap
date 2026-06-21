0e42461fbee67f85e41df0f5f6c7482c36ee87d6
Ahmed Janabi <ahmed541991@gmail.com>
2026-06-21 13:12:46 +0400

    decaffeinate 1.4.0

--- Casks/decaffeinate.rb
+++ Casks/decaffeinate.rb
@@ -3,8 +3,8 @@
 # and `sha256` together on each release (the sha is printed by
 # Scripts/make-dmg.sh and published as SHA256SUMS.txt on the GitHub release).
 cask "decaffeinate" do
-  version "1.3.0"
-  sha256 "71b727fcedf516014abc0e6f27c7a7f2a9d8630990db4285ee09bfeac337bfc5"
+  version "1.4.0"
+  sha256 "60bab12ab29927fbe36197dc067c58c160ca12a779be84ae03aa50636269a9db"
 
   url "https://github.com/harf-promo/decaffeinate/releases/download/v#{version}/Decaffeinate-#{version}.dmg"
   name "Decaffeinate"
